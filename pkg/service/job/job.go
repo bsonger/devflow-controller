46eb3ad6847c5c1f6b016565451e87fd1477e43e
bei song <bsonger@163.com>
2026-01-06 06:02:23 +0800

    fix update status bug

--- pkg/service/job/job.go
+++ pkg/service/job/job.go
@@ -5,6 +5,7 @@ import (
 	"github.com/bsonger/devflow-common/client/mongo"
 	"github.com/bsonger/devflow-common/model"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
@@ -14,14 +15,21 @@ type jobService struct {
 }
 
 func (s *jobService) UpdateJobStatus(ctx context.Context, jobID string, status model.JobStatus) error {
+	// 1️⃣ 把 string ID 转成 ObjectID
+	oid, err := primitive.ObjectIDFromHex(jobID)
+	if err != nil {
+		return err
+	}
 
+	// 2️⃣ 构造 filter
 	filter := bson.M{
-		"id": jobID,
+		"_id": oid,
 		"status": bson.M{
 			"$nin": []model.JobStatus{model.JobSucceeded, model.JobFailed, status},
 		},
 	}
 
+	// 3️⃣ 执行更新
 	return mongo.Repo.UpdateOne(
 		ctx,
 		&model.Job{},
